9070188790da6b3af39092d6a7ec639819d326d9
Lívia <liviaribeiro14@gmail.com>
2019-06-23 11:53:29 -0300

    Ordenação e inversão dos valores da tabela.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,12 +9,14 @@ class NegociacaoController{
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(), 
             new NegociacoesView($('#negociacoesView')), 
-            'adiciona','esvazia');
+            'adiciona','esvazia', 'ordena', 'inverteOrdem');
 
         this._mensagem = new Bind(
             new Mensagem(), 
             new MensagemView($('#mensagemView')), 
             'texto');
+
+        this._ordemAtual = '';
     }
 
     adiciona(event){
@@ -24,6 +26,15 @@ class NegociacaoController{
         this._limpaFormulario();
 
         this._mensagem.texto = "Negociação incluída";
+    }
+
+    ordena(coluna){
+        if(this._ordemAtual == coluna){
+            this._listaNegociacoes.inverteOrdem();
+        }else{
+            this._listaNegociacoes.ordena((a,b) => a[coluna] - b[coluna]);
+        }
+        this._ordemAtual = coluna;
     }
 
     importarNegociacoes(){
